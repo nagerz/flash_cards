cf6c02cc954ad84cb43cea241b267df7710a0b27
nagerz <znager@gmail.com>
2018-11-29 13:35:22 -0700

    Fill our runner file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,12 +3,27 @@ require './lib/deck'
 require './lib/round'
 require './lib/turn'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_1 = Card.new("What is 5 + 5?", "10", :Math)
 card_2 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_3 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_4 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-cards = [card_1, card_2, card_3]
+cards = [card_1, card_2, card_3, card_4]
 deck = Deck.new(cards)
 round = Round.new(deck)
 
-puts "Welcome! You're playing with 4 cards."
+round.start
+cards.each do
+  #round.@turns.count < round.@deck.count
+  puts "This is card number #{round.turns.count + 1} out of #{round.deck.count}"
+  puts "Question: #{round.current_card.question}?"
+  guess = gets.chomp.to_s
+  puts round.take_turn(guess).feedback
+end
+puts ""
+round.end
+puts "You had #{round.number_correct} correct guesses out of #{round.turns.count}
+for a total score of #{round.percent_correct}%."
+puts "STEM - #{round.percent_correct_by_category(:STEM)}% correct"
+puts "Geography - #{round.percent_correct_by_category(:Geography)}% correct"
+puts "Math - #{round.percent_correct_by_category(:Math)}% correct"
+puts "Pop Culture - #{round.percent_correct_by_category(:Pop_Culture)}% correct"
