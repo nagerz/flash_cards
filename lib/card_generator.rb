61a5987cca60a46c637e9a94d1b79911ac31ef46
nagerz <znager@gmail.com>
2018-11-30 16:21:50 -0700

    Clean up Card_generator and comment

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -7,9 +7,12 @@ class CardGenerator
   end
 
   def cards
+    #Create empty array for cards from text file to be placed (deck).
     textcards = []
-    #text = File.open(@textfile)
+    #Create array stings that represent each line from text file.
     card_substrings = IO.readlines(@textfile)
+    #Seperate answer, question, and category from each line substring,
+    #create card array, place card arrays in single array which is returned.
     card_substrings.each do |n|
       info_substrings = n.split(",")
       card = Card.new(info_substrings[0], info_substrings[1], info_substrings[2])
@@ -17,5 +20,4 @@ class CardGenerator
     end
       return textcards
   end
-
 end
